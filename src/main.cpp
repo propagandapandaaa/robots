c981d612b053f19dd6b20542c8ec8bdee27392aa
jacobs <daniel.jacobs@outlook.cz>
2025-02-26 16:08:49 +0100

    test: rebase

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@ int main()
     std::cout << "oiiiaa oiiia" << std::endl;
     std::cout << "conflictos duos" << std::endl;
     std::cout << "LIBERALS RULE!" << std::endl;
+    std::cout << "conflictos unos" << std::endl;
     YourMom YourMom;
     std::cout << YourMom.capNoCapMemeDrip(9000) << std::endl;
     std::cout << YourMom.skibidiRoastRizz("sigma") << std::endl;
