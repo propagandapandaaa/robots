a60322837a836571f6103d2db5e7c7dc3862f4f1
jacobs <daniel.jacobs@outlook.cz>
2025-02-26 16:09:57 +0100

    test: conflict dev branch

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 int main()
 {
     std::cout << "oiiiaa oiiia" << std::endl;
+    std::cout << "conflictos duos" << std::endl;
     YourMom YourMom;
     std::cout << YourMom.capNoCapMemeDrip(9000) << std::endl;
     std::cout << YourMom.skibidiRoastRizz("sigma") << std::endl;
