2d6536c7102edb3c01726993f70dee9313faff50
Tomáš Hobza <hobza.tomas@gmail.com>
2025-02-26 16:00:02 +0100

    add: print

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 
 int main()
 {
+    std::cout << "oiiiaa oiiia" << std::endl;
     YourMom YourMom;
     std::cout << YourMom.capNoCapMemeDrip(9000) << std::endl;
     std::cout << YourMom.skibidiRoastRizz("sigma") << std::endl;
