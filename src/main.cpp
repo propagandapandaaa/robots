bf608942641f3db483abde41ecc87265507529d2
jacobs <daniel.jacobs@outlook.cz>
2025-02-26 15:37:43 +0100

    add: basic project structure

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "../include/robots/lib.hpp"
+
+#include <iostream>
+
+int main()
+{
+    YourMom YourMom;
+    std::cout << YourMom.capNoCapMemeDrip(9000) << std::endl;
+    std::cout << YourMom.skibidiRoastRizz("sigma") << std::endl;
+}
